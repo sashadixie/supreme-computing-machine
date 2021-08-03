293399428fd6bfff51d799234a7bc2d34ef4e107
aleksandra.sokolova@rtlabs.ru <dixxiesasha@gmail.com>
2021-08-03 16:15:35 +0300

    express

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import cors from 'cors';
+import fetch from 'cross-fetch';
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const __dirname = path.resolve();
+app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.use(cors());
+
+app.get('/api', (req, res) => {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => response.json())
+    .then((json) => res.render('api', { items: json }));
+});
+
+app.listen("3000", () =>
+  console.log(`Example app listening on port 3000!`),
+);
